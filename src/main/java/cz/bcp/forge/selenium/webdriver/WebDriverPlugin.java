ee568a8f4b1f43fc3fa436c4503f2447973439ff
Jan Holman <jenyk.holman@gmail.com>
2013-10-20 23:13:25 +0200

    testCRUD pro String pole

--- src/main/java/cz/bcp/forge/selenium/webdriver/WebDriverPlugin.java
+++ src/main/java/cz/bcp/forge/selenium/webdriver/WebDriverPlugin.java
@@ -2,6 +2,9 @@ package cz.bcp.forge.selenium.webdriver;
 
 import java.io.FileNotFoundException;
 import java.io.StringWriter;
+import org.jboss.forge.parser.java.Field;
+import org.jboss.forge.parser.java.Member;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import javax.enterprise.event.Event;
@@ -17,6 +20,9 @@ import org.jboss.forge.project.dependencies.DependencyBuilder;
 import org.jboss.forge.project.dependencies.ScopeType;
 import org.jboss.forge.project.facets.DependencyFacet;
 import org.jboss.forge.project.facets.JavaSourceFacet;
+import org.jboss.forge.project.facets.MetadataFacet;
+import org.jboss.forge.project.facets.WebResourceFacet;
+import org.jboss.forge.resources.java.JavaFieldResource;
 import org.jboss.forge.resources.java.JavaResource;
 import org.jboss.forge.shell.PromptType;
 import org.jboss.forge.shell.Shell;
@@ -33,30 +39,30 @@ import org.jboss.forge.shell.plugins.SetupCommand;
  */
 @Alias("webdriver")
 public class WebDriverPlugin implements Plugin {
-	
-	public static final String JUNIT_VERSION_PROP_NAME = "version.junit";
-	public static final String JUNIT_VERSION_PROP = "${" + JUNIT_VERSION_PROP_NAME + "}";
-	
-	public static final String SELENIUM_VERSION_PROP_NAME = "version.selenium";
-	public static final String SELENIUM_VERSION_PROP = "${" + SELENIUM_VERSION_PROP_NAME + "}";
-	
-	@Inject
-	private Shell shell;
 
-	@Inject
-	private Project project;
+    public static final String JUNIT_VERSION_PROP_NAME = "version.junit";
+    public static final String JUNIT_VERSION_PROP = "${" + JUNIT_VERSION_PROP_NAME + "}";
+
+    public static final String SELENIUM_VERSION_PROP_NAME = "version.selenium";
+    public static final String SELENIUM_VERSION_PROP = "${" + SELENIUM_VERSION_PROP_NAME + "}";
+
+    @Inject
+    private Shell shell;
+
+    @Inject
+    private Project project;
 
     @Inject
     private Event<PickupResource> pickup;
 
-	private DependencyFacet dependencyFacet;
+    private DependencyFacet dependencyFacet;
 
-	@SetupCommand
-	public void setupCommand() {
-		dependencyFacet = project.getFacet(DependencyFacet.class);
-		installJunitDependencies();
-		installWebDriverDependencies();
-	}
+    @SetupCommand
+    public void setupCommand() {
+        dependencyFacet = project.getFacet(DependencyFacet.class);
+        installJunitDependencies();
+        installWebDriverDependencies();
+    }
 
     static {
         Properties properties = new Properties();
@@ -67,19 +73,31 @@ public class WebDriverPlugin implements Plugin {
         Velocity.init(properties);
     }
 
-    @Command(value = "create-test", help = "Create a new test class with a default @Deployment method")
+    @Command(value = "create-test", help = "Create a new test class")
     public void createTest(@Option(name = "class", required = true, type = PromptType.JAVA_CLASS) JavaResource classUnderTest,
                            final PipeOut out)
             throws FileNotFoundException {
         JavaSourceFacet java = project.getFacet(JavaSourceFacet.class);
-
+        WebResourceFacet web = project.getFacet(WebResourceFacet.class);
+        MetadataFacet metaData = project.getFacet(MetadataFacet.class);
         JavaSource<?> javaSource = classUnderTest.getJavaSource();
+
+        List members = javaSource.getMembers();
+        List fields = new ArrayList<Field>();
+        for (Member<?, ?> member : javaSource.getMembers()) {
+            if (member instanceof Field) {
+                fields.add(member);
+            }
+        }
 
         VelocityContext context = new VelocityContext();
         context.put("package", javaSource.getPackage());
         context.put("ClassToTest", javaSource.getName());
         context.put("classToTest", javaSource.getName().toLowerCase());
         context.put("packageImport", javaSource.getPackage());
+        context.put("fields", fields);
+        context.put("projectName", metaData.getProjectName());
+
 
         StringWriter writer = new StringWriter();
         Velocity.mergeTemplate("TestTemplate.vtl", "UTF-8", context, writer);
@@ -89,38 +107,38 @@ public class WebDriverPlugin implements Plugin {
 
         pickup.fire(new PickupResource(java.getTestJavaResource(testClass)));
     }
-	
-	private void installJunitDependencies() {
-		DependencyBuilder junitDependency = createJunitDependency();
-		if (!dependencyFacet.hasEffectiveDependency(junitDependency)) {
-			List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(junitDependency);
-			Dependency dependency = shell.promptChoiceTyped("Which version of JUnit do you want to install?", dependencies,
-					DependencyUtil.getLatestNonSnapshotVersion(dependencies));
 
-			dependencyFacet.setProperty(JUNIT_VERSION_PROP_NAME, dependency.getVersion());
-			dependencyFacet.addDirectDependency(DependencyBuilder.create(dependency).setVersion(JUNIT_VERSION_PROP));
-		}
+    private void installJunitDependencies() {
+        DependencyBuilder junitDependency = createJunitDependency();
+        if (!dependencyFacet.hasEffectiveDependency(junitDependency)) {
+            List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(junitDependency);
+            Dependency dependency = shell.promptChoiceTyped("Which version of JUnit do you want to install?", dependencies,
+                    DependencyUtil.getLatestNonSnapshotVersion(dependencies));
 
-	}
-	
-	private void installWebDriverDependencies() {
-		DependencyBuilder webDriverDependency = createWebDriverDependency();
-		if (!dependencyFacet.hasEffectiveDependency(webDriverDependency)) {
-			List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(webDriverDependency);
-			Dependency dependency = shell.promptChoiceTyped("Which version of Selenium do you want to install?", dependencies,
-					DependencyUtil.getLatestNonSnapshotVersion(dependencies));
+            dependencyFacet.setProperty(JUNIT_VERSION_PROP_NAME, dependency.getVersion());
+            dependencyFacet.addDirectDependency(DependencyBuilder.create(dependency).setVersion(JUNIT_VERSION_PROP));
+        }
 
-			dependencyFacet.setProperty(SELENIUM_VERSION_PROP_NAME, dependency.getVersion());
-			dependencyFacet.addDirectDependency(DependencyBuilder.create(dependency).setVersion(SELENIUM_VERSION_PROP));
-		}
+    }
 
-	}
+    private void installWebDriverDependencies() {
+        DependencyBuilder webDriverDependency = createWebDriverDependency();
+        if (!dependencyFacet.hasEffectiveDependency(webDriverDependency)) {
+            List<Dependency> dependencies = dependencyFacet.resolveAvailableVersions(webDriverDependency);
+            Dependency dependency = shell.promptChoiceTyped("Which version of Selenium do you want to install?", dependencies,
+                    DependencyUtil.getLatestNonSnapshotVersion(dependencies));
 
-	private DependencyBuilder createJunitDependency() {
-		return DependencyBuilder.create().setGroupId("junit").setArtifactId("junit").setScopeType(ScopeType.TEST);
-	}
-	
-	private DependencyBuilder createWebDriverDependency() {
-		return DependencyBuilder.create().setGroupId("org.seleniumhq.selenium").setArtifactId("selenium-java").setScopeType(ScopeType.TEST);
-	}
+            dependencyFacet.setProperty(SELENIUM_VERSION_PROP_NAME, dependency.getVersion());
+            dependencyFacet.addDirectDependency(DependencyBuilder.create(dependency).setVersion(SELENIUM_VERSION_PROP));
+        }
+
+    }
+
+    private DependencyBuilder createJunitDependency() {
+        return DependencyBuilder.create().setGroupId("junit").setArtifactId("junit").setScopeType(ScopeType.TEST);
+    }
+
+    private DependencyBuilder createWebDriverDependency() {
+        return DependencyBuilder.create().setGroupId("org.seleniumhq.selenium").setArtifactId("selenium-java").setScopeType(ScopeType.TEST);
+    }
 }
